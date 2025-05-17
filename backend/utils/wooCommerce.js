f425ad25c54d64cb867932be6a23a12685948e64
mern-mnnlr-Ashootosh <askrbm47@gmail.com>
2025-05-18 04:36:21 +0530

    Fix WooCommerce API config and environment variables

--- backend/utils/wooCommerce.js
+++ backend/utils/wooCommerce.js
@@ -1,12 +1,10 @@
-if(process.env.NODE_ENV != "production") {
-    require('dotenv').config();
-}
+
 const WooCommerceRestApi = require('@woocommerce/woocommerce-rest-api').default;
 
 const WooCommerce = new WooCommerceRestApi({
   url:'https://ecom-softly-hard-blaze.wpcomstaging.com',
-  consumerKey: process.env.WooCommerce_ConsumerKey,
-  consumerSecret: process.env.WooCommerce_ConsumerSecret,
+  consumerKey: 'ck_45f56aef41d13094a491ac745243e9a11080476b',
+  consumerSecret:'cs_1ff8f0783046758ae7c675a0120e2fad1ff3c189',
   version: 'wc/v3',
   queryStringAuth: true,
 });
